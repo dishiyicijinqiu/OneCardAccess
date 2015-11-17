190a2897baac689e3c5829637c08de86d492b698
dishiyicijinqiu <458141536@qq.com>
2015-11-17 21:30:43 +0800

    wcf异常处理

--- Application.IntegeatedManageServer.Config/ApplicationConfig.cs
+++ Application.IntegeatedManageServer.Config/ApplicationConfig.cs
@@ -7,5 +7,29 @@ namespace FengSharp.OneCardAccess.Application.IntegeatedManageServer.Config
 {
     public class ApplicationConfig
     {
+        private static int _SessionTimeOutMinutes = -1;
+        public static int SessionTimeOutMinutes
+        {
+            get
+            {
+                if (_SessionTimeOutMinutes == -1)
+                {
+                    _SessionTimeOutMinutes = Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["SessionTimeOutMinutes"]);
+                }
+                return _SessionTimeOutMinutes;
+            }
+        }
+        private static string _SessionCacheName;
+        public static string SessionCacheName
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_SessionCacheName))
+                {
+                    _SessionCacheName = System.Configuration.ConfigurationManager.AppSettings["SessionCacheName"];
+                }
+                return _SessionCacheName;
+            }
+        }
     }
 }
