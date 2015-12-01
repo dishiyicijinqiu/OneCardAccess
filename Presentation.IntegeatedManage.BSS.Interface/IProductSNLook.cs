4968dfc628f6d53b207f5437997c214e8d752b29
dishiyicijinqiu <458141536@qq.com>
2015-12-01 19:18:19 +0800

    1

---
+++ Presentation.IntegeatedManage.BSS.Interface/IProductSNLook.cs
@@ -0,0 +1,10 @@
+﻿
+using FengSharp.OneCardAccess.Domain.BSSModule.Entity;
+
+namespace FengSharp.OneCardAccess.Presentation.IntegeatedManage.BSS.Interface
+{
+    public interface IProductSNLook
+    {
+        void BindData(PSNInOutDetailsEntity[] data);
+    }
+}
---
+++ Presentation.IntegeatedManage.BSS/Dlys/Controls/ProductFBNLookPopupContainerControl.cs
@@ -0,0 +1,39 @@
+﻿using DevExpress.XtraEditors;
+using FengSharp.OneCardAccess.Domain.BSSModule.Entity;
+using FengSharp.OneCardAccess.Presentation.IntegeatedManage.BSS.Interface;
+
+namespace FengSharp.OneCardAccess.Presentation.IntegeatedManage.BSS
+{
+    public class ProductFBNLookPopupContainerControl : PopupContainerControl, IProductFBNLook
+    {
+        private ProductFBNLookUserControl selectUserControl1;
+        public ProductFBNLookPopupContainerControl()
+        {
+            InitializeComponent();
+        }
+        private void InitializeComponent()
+        {
+            this.selectUserControl1 = new ProductFBNLookUserControl();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectUserControl1
+            // 
+            this.selectUserControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.selectUserControl1.Location = new System.Drawing.Point(0, 0);
+            this.selectUserControl1.Name = "selectUserControl1";
+            this.selectUserControl1.Size = new System.Drawing.Size(200, 100);
+            this.selectUserControl1.TabIndex = 0;
+            // 
+            // ProductFBNLookPopupContainerControl
+            // 
+            this.Controls.Add(this.selectUserControl1);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+        }
+        public void BindData(PFBNInOutDetailsEntity[] data)
+        {
+            this.selectUserControl1.BindData(data);
+        }
+    }
+}
